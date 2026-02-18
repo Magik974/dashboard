84b34ad3ab6390b8d459de102d94a93feeb5f1fd
malick mdahoma <malick.mdahoma@groupama.com>
2026-02-18 19:07:40 +0100

    Refactor invoice fetching logic to use Supabase and enhance search functionality with debounced input

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -171,27 +171,50 @@ export async function fetchFilteredInvoices(
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
-    const invoices = await sql<InvoicesTable[]>`
-      SELECT
-        invoices.id,
-        invoices.amount,
-        invoices.date,
-        invoices.status,
-        customers.name,
-        customers.email,
-        customers.image_url
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      WHERE
-        customers.name ILIKE ${`%${query}%`} OR
-        customers.email ILIKE ${`%${query}%`} OR
-        invoices.amount::text ILIKE ${`%${query}%`} OR
-        invoices.date::text ILIKE ${`%${query}%`} OR
-        invoices.status ILIKE ${`%${query}%`}
-      ORDER BY invoices.date DESC
-      LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
-    `;
+    // const invoices = await sql<InvoicesTable[]>`
+    //   SELECT
+    //     invoices.id,
+    //     invoices.amount,
+    //     invoices.date,
+    //     invoices.status,
+    //     customers.name,
+    //     customers.email,
+    //     customers.image_url
+    //   FROM invoices
+    //   JOIN customers ON invoices.customer_id = customers.id
+    //   WHERE
+    //     customers.name ILIKE ${`%${query}%`} OR
+    //     customers.email ILIKE ${`%${query}%`} OR
+    //     invoices.amount::text ILIKE ${`%${query}%`} OR
+    //     invoices.date::text ILIKE ${`%${query}%`} OR
+    //     invoices.status ILIKE ${`%${query}%`}
+    //   ORDER BY invoices.date DESC
+    //   LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
+    // `;
+    //use supabase api to fetch filtered invoices with
 
+    console.log('Fetching filtered invoices with query:', ITEMS_PER_PAGE, 'and page:', currentPage);
+    const invoicesSearch = await supabase.rpc('search_invoices', {
+      _query: query,
+      _limit: ITEMS_PER_PAGE,
+      _offset: offset,
+    });
+
+
+    console.log('Fetched filtered invoices:', invoicesSearch);
+
+    const invoices: InvoicesTable[] = invoicesSearch.data?.map((invoice: any) => ({
+      id: invoice.id,
+      amount: invoice.amount,
+      date: invoice.date,
+      status: invoice.status,
+      name: invoice.customers?.[0]?.name ?? '',
+      email: invoice.customers?.[0]?.email ?? '',
+      image_url: invoice.customers?.[0]?.image_url ?? '',
+    })) ?? [];
+
+
+    console.log('Fetched invoices:', invoices.length);
     return invoices;
   } catch (error) {
     console.error('Database Error:', error);
@@ -201,18 +224,28 @@ export async function fetchFilteredInvoices(
 
 export async function fetchInvoicesPages(query: string) {
   try {
-    const data = await sql`SELECT COUNT(*)
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE
-      customers.name ILIKE ${`%${query}%`} OR
-      customers.email ILIKE ${`%${query}%`} OR
-      invoices.amount::text ILIKE ${`%${query}%`} OR
-      invoices.date::text ILIKE ${`%${query}%`} OR
-      invoices.status ILIKE ${`%${query}%`}
-  `;
+    //   const data = await sql`SELECT COUNT(*)
+    //   FROM invoices
+    //   JOIN customers ON invoices.customer_id = customers.id
+    //   WHERE
+    //     customers.name ILIKE ${`%${query}%`} OR
+    //     customers.email ILIKE ${`%${query}%`} OR
+    //     invoices.amount::text ILIKE ${`%${query}%`} OR
+    //     invoices.date::text ILIKE ${`%${query}%`} OR
+    //     invoices.status ILIKE ${`%${query}%`}
+    // `;
 
-    const totalPages = Math.ceil(Number(data[0].count) / ITEMS_PER_PAGE);
+    const { data, error } = await supabase
+      .rpc('count_search_invoices', {
+        _query: query
+
+      })
+    if (error) console.error(error)
+    else console.log('Fetched total invoices count for pagination:', data);
+
+    const InvoicesTotalNumber = data ?? 0;
+
+    const totalPages = Math.ceil(Number(InvoicesTotalNumber / ITEMS_PER_PAGE));
     return totalPages;
   } catch (error) {
     console.error('Database Error:', error);
